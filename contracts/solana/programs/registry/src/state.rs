67fcb7e6e5255b6a9b023b5d58dd9f730015e39a
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2024-11-28 14:47:46 +0100

    add Levy account

--- contracts/solana/programs/registry/src/state.rs
+++ contracts/solana/programs/registry/src/state.rs
@@ -4,14 +4,20 @@ use anchor_lang::prelude::*;
 pub struct SchemaData {
     /// Generate PDA as reference key.
     pub uid: Pubkey,
+
     /// The actual schema data (e.g., JSON, XML, etc.).
     pub schema: String,
+
     /// Resolver address (another contract) for schema verification.
     pub resolver: Option<Pubkey>,
+
     /// Indicates whether the schema is revocable.
     pub revocable: bool,
+
     /// The deployer/authority who created the schema.
     pub deployer: Pubkey,
+
+    pub levy: Option<Levy>,
 }
 
 impl SchemaData {
@@ -21,4 +27,17 @@ impl SchemaData {
     // 200 bytes for schema string,
     // 32 bytes for deployer pubkey.
     pub const LEN: usize = 8 + 32 + 1 + 200 + 32;
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Levy {
+    /// 8 bytes
+    pub amount: u64,
+
+    /// 32 bytes (Asset address)
+    pub asset: Option<Pubkey>,
+
+    /// 32 bytes (Recipient of the levy)
+    pub recipient: Pubkey,
 }
