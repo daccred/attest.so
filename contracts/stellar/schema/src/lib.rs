55b584405a2a2ef3bf8726a2710c08caace30b5c
Perelyn <okohebina@gmail.com>
2025-04-05 00:48:24 +0100

    refactor schema register func

--- contracts/stellar/schema/src/lib.rs
+++ contracts/stellar/schema/src/lib.rs
@@ -1,23 +1,8 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Address, Env, String, Vec};
 
-#[derive(Debug, Clone)]
-pub struct Schema {
-    pub uid: Vec<u8>,
-    pub name: String,
-    pub definition: String,
-    pub resolver: Option<Address>,
-    pub revocable: bool,
-    pub authority: Address,
-    pub levy: Option<Levy>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Levy {
-    pub amount: u64,
-    pub asset: Address,
-    pub recipient: Address,
-}
+use soroban_sdk::{
+    contract, contracterror, contractimpl, contracttype, symbol_short, Address, Bytes, Env, String,
+};
 
 #[derive(Debug)]
 pub struct Authority {
@@ -25,73 +10,72 @@ pub struct Authority {
     pub metadata: String,
 }
 
-pub trait SchemaContract {
-    type Error;
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    LimitReached = 1,
+}
 
-    fn schema_created(schema: Schema);
+#[contracttype]
+pub enum DataKey {
+    MyKey,
+}
 
-    fn create_schema(
-        name: String,
-        definition: String,
-        resolver: Option<Address>,
-        revocable: bool,
-        levy: Option<Levy>,
-    ) -> Result<(), Self::Error>;
+#[derive(Debug, Clone)]
+#[contracttype]
+pub struct Schema {
+    uid: Bytes,
+    schema: String,
+    resolver: Option<Address>,
+    revocable: bool,
+}
+
+#[derive(Debug, Clone)]
+#[contracttype]
+pub struct Levy {
+    pub amount: u64,
+    pub asset: Address,
+    pub recipient: Address,
 }
 
 #[contract]
-pub struct SchemaContractImpl;
+pub struct SchemaContract;
 
 #[contractimpl]
-impl SchemaContract for SchemaContractImpl {
-    type Error = String;
-
-    fn schema_created(env: Env, schema: Schema) {
-        env.emit().schema_created(schema);
-    }
-
-    fn create_schema(
+impl SchemaContract {
+    pub fn register(
         env: Env,
-        name: String,
-        definition: String,
+        caller: Address,
+        schema: String,
         resolver: Option<Address>,
         revocable: bool,
-        levy: Option<Levy>,
-    ) -> Result<(), Self::Error> {
-        // Ensure the caller is a registered authority
-        let caller = env.caller();
-        let authority = authorities(env, caller).expect("Caller is not a registered authority");
+    ) -> Result<(), Error> {
+        caller.require_auth();
 
-        // Generate a unique schema UID
-        let uid = env.hash(&name);
+        // generate unique uid with caller, schema and resolver
+        let slice = &mut [0u8; 32];
+        schema.copy_into_slice(slice);
+        caller.to_string().copy_into_slice(slice);
+        if let Some(address) = &resolver {
+            address.to_string().copy_into_slice(slice);
+        }
+        let uid = env.crypto().keccak256(&Bytes::from_slice(&env, slice));
 
         // Create the schema
         let schema = Schema {
-            uid: uid.to_vec(),
-            name,
-            definition,
+            uid: uid.to_bytes().try_into().unwrap(), // remove unwrap
+            schema,
             resolver,
             revocable,
-            authority: caller,
-            levy,
         };
 
-        // Store the schema
-        schemas(env, uid).set(&schema)?;
+        env.storage().instance().set(&DataKey::MyKey, &schema);
 
-        // Emit the SchemaCreated event
-        env.emit().schema_created(schema.clone());
-
-        // Checks if the caller is a registered authority
-        fn authorities(env: Env, address: Address) -> Option<Authority> {
-            env.storage().get(address)
-        }
+        env.events()
+            .publish((symbol_short!("schema"), symbol_short!("register")), schema);
 
         Ok(())
     }
 }
-
-// Storage functions
-fn schemas(env: Env, uid: Vec<u8>) -> Option<Schema> {
-    env.storage().get(uid)
-}
+mod test;
