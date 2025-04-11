51abace5e3f37b73f9cd68e3ed35b2b727fc5336
Paul Imoke <pauleke65@gmail.com>
2025-04-11 17:41:50 +0100

    feat (sdk): add multi publish options npm, umd, deno

---
+++ packages/sdk/webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist/umd'),
+    filename: 'attestsdk.js',
+    library: {
+      type: 'umd',
+      name: 'attestsdk',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      process: 'process/browser',
+    }),
+  ],
+};
