553a6900182bf85eb3b2e2e9ac79dc6138eacbee
Andrew Miracle <me@andrewmiracle.com>
2025-05-16 08:26:53 +0000

    ci(www): fix layout does not match the required types of a Next.js Layout

--- apps/www/app/(home)/layout.tsx
+++ apps/www/app/(home)/layout.tsx
@@ -1,10 +1,10 @@
 import { Fragment, type ReactNode } from 'react'
 import { HomeLayout } from 'fumadocs-ui/home-layout'
-import { baseOptions } from '../layout.config'
+// import { baseOptions } from '../layout.config'
 
-export function ExtendedHomeLayout({ children }: { children: ReactNode }): React.ReactElement {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>
-}
+// export function ExtendedHomeLayout({ children }: { children: ReactNode }): React.ReactElement {
+//   return <HomeLayout {...baseOptions}>{children}</HomeLayout>
+// }
 export default function Layout({ children }: { children: ReactNode }): React.ReactElement {
   return <Fragment>{children}</Fragment>
 }
