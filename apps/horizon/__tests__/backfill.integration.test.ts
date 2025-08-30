63ecc02e0f2b4c149389a318dd77e859f8e06d85
Andrew Miracle <me@andrewmiracle.com>
2025-08-30 03:49:21 +0000

    test(horizon): update backfill integration test ledger range
    Update test ledger range from 200088-245199 to 245199-266127 to test more recent ledger data

--- apps/horizon/__tests__/backfill.integration.test.ts
+++ apps/horizon/__tests__/backfill.integration.test.ts
@@ -7,8 +7,8 @@ import { PrismaClient } from '@prisma/client'
 
 // Integration test timeout - backfill operations can take time
 const BACKFILL_TIMEOUT = 300000
-const BACKFILL_START_LEDGER = 200088
-const BACKFILL_END_LEDGER = 245199
+const BACKFILL_START_LEDGER = 245199
+const BACKFILL_END_LEDGER = 266127
 
 describe('Backfill Integration Test', () => {
   let db: any
